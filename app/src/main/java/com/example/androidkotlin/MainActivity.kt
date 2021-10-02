d0da018fa29b7368a1fbb24a49dd5ac5fe72b353
Dmitriev_Dmitry <dimonkudo@gmail.com>
2021-10-02 15:59:48 +0300

    3) Перевел MainActivity на Котлин

--- app/src/main/java/com/example/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlin/MainActivity.kt
@@ -1,13 +1,12 @@
-package com.example.androidkotlin;
+package com.example.androidkotlin
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidkotlin.R
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
