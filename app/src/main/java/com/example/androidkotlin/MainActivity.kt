9a638cbb0b91f74dad5bdc0a3c31f00fa64333dc
Dmitriev_Dmitry <dimonkudo@gmail.com>
2021-10-02 16:24:43 +0300

    4) Добавить кнопку в разметку и повесить на неё clickListener в Activity.

--- app/src/main/java/com/example/androidkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlin/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.androidkotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import com.example.androidkotlin.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById(R.id.first_button)
+        button.setOnClickListener(buttonClick)
     }
+
+    var buttonClick = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.first_button -> {
+                Toast.makeText(this@MainActivity, "Its toast!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
 }
